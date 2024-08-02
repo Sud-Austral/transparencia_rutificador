3802f4d1acfc2bcd40a9d6502bcb04cd6aabc407
lmonsalve22 <lmonsalve22@gmail.com>
2024-08-02 14:10:49 -0400

    mostrar data

--- build1.py
+++ build1.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import urllib.parse
+
+def string_to_url(s):
+    return urllib.parse.quote(s)
 
 base = "https://www.cplt.cl/transparencia_activa/datoabierto/archivos/"
 deseadas =["Nombres","Paterno","Materno","organismo_nombre",'anyo', 'Mes','tipo_calificacionp']
@@ -15,9 +19,42 @@ PersonalContrataDICT              = deseadas+["remuliquida_mensual",'Tipo cargo'
 PersonalCodigotrabajoDICT         = deseadas+["remuliquida_mensual",'Tipo cargo', 'remuneracionbruta_mensual']
 PersonalContratohonorariosDICT    = deseadas+['remuliquida_mensual','tipo_pago','num_cuotas','remuneracionbruta']
 
-
+comunas = ['Corporación Municipal de Providencia',
+ 'Municipalidad de Antofagasta',
+ 'Municipalidad de Chillán',
+ 'Municipalidad de Concepción',
+ 'Municipalidad de Coquimbo',
+ 'Municipalidad de Curicó',
+ 'Municipalidad de El Bosque',
+ 'Municipalidad de Florida',
+ 'Municipalidad de Iquique',
+ 'Municipalidad de La Pintana',
+ 'Municipalidad de La Serena',
+ 'Municipalidad de Los Ángeles',
+ 'Municipalidad de Maipú',
+ 'Municipalidad de Peñalolén',
+ 'Municipalidad de Pudahuel',
+ 'Municipalidad de Puente Alto',
+ 'Municipalidad de Puerto Montt',
+ 'Municipalidad de Quilicura',
+ 'Municipalidad de Rancagua',
+ 'Municipalidad de San Bernardo',
+ 'Municipalidad de Santiago',
+ 'Municipalidad de Talca',
+ 'Municipalidad de Talcahuano',
+ 'Municipalidad de Valdivia',
+ 'Municipalidad de Valparaíso',
+ 'Municipalidad de Viña del Mar',
+ 'Municipalidad de Ñuñoa']
 
 
 
 if __name__ == '__main__':
+    #https://github.com/Sud-Austral/BASE_COMUNAS_TRANSPARENCIA/raw/main/comunas/Corporaci%C3%B3n%20Municipal%20de%20Providencia.csv
     print("Hola")
+    for i in comunas:
+        base = string_to_url(i)
+        url = f"https://github.com/Sud-Austral/BASE_COMUNAS_TRANSPARENCIA/raw/main/comunas/{base}.csv"
+        df = pd.read_csv(url, compression='xz', sep='\t')
+        print(url)
+        print(df)
