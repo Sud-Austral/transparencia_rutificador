30df02eef1a85d64c4eca1fc7d9c8556954d8202
lmonsalve22 <lmonsalve22@gmail.com>
2024-08-02 14:35:00 -0400

    getpagos

--- build1.py
+++ build1.py
@@ -65,6 +65,12 @@ comunas = ['Corporación Municipal de Providencia',
 def string_to_url(s):
     return urllib.parse.quote(s)
 
+def fixRemuneracion(valor):
+    try:
+        return int(valor[:-2])
+    except:
+        None
+
 def eliminar_espacios_adicionales(cadena):
     if(type(cadena) == float):
         return "NO"
@@ -93,16 +99,38 @@ def get_nombre_completo(df):
         )
     return df
 
-
+def getPagos(df):    
+    """
+    Process payments data from an Excel file and save the results to a new file.
+    
+    Parameters:
+    URL (str): The file path of the Excel file to process.
+    input_dir (str): The directory name in the input file path to replace (default is "organismoSalida2").
+    output_dir (str): The directory name in the output file path to replace (default is "organismoSalida3").
+    """
+    result = df[['anyo', 'Mes','rut']].apply(lambda row: '-'.join(row.values.astype(str)), axis=1)
+    df["año-mes-rut"] = result
+    resultados = pd.DataFrame(result.value_counts()).reset_index().rename(columns={'count':'Cantidad de pagos en un mes',0: 'Cantidad de pagos en un mes',"index":"año-mes-rut"})
+    df2 = df.merge(resultados)
+    result = df2[["año-mes-rut",'base']].apply(lambda row: '-'.join(row.values.astype(str)), axis=1)
+    df2["año-mes-rut-base"] = result
+    resultados = pd.DataFrame(result.value_counts()).reset_index().rename(columns={'count':'Detalle de base en pagos en un mes',0: 'Detalle de base en pagos en un mes',"index":"año-mes-rut-base"})
+    df2 = df2.merge(resultados)
+    distinct_counts = df2[["año-mes-rut","año-mes-rut-base"]].groupby("año-mes-rut")["año-mes-rut-base"].nunique().reset_index()
+    distinct_counts.columns = ["año-mes-rut", "Tipo de contrato distintos"]
+    df2 = df2.merge(distinct_counts)    
+    df2['remuliquida_mensual'] = df2['remuliquida_mensual'].map(fixRemuneracion)
+    df2['remuneracionbruta_mensual'] = df2['remuneracionbruta_mensual'].map(fixRemuneracion)
+    return df2
 
 if __name__ == '__main__':
     #https://github.com/Sud-Austral/BASE_COMUNAS_TRANSPARENCIA/raw/main/comunas/Corporaci%C3%B3n%20Municipal%20de%20Providencia.csv
-    print("Hola")
     for i in comunas:
         base = string_to_url(i)
         url = f"https://github.com/Sud-Austral/BASE_COMUNAS_TRANSPARENCIA/raw/main/comunas/{base}.csv"
         df = pd.read_csv(url, compression='xz', sep='\t')
-        df2 = get_nombre_completo(df)
-        df2.to_excel(f"test/{i}.xlsx", index=False)
+        df = get_nombre_completo(df)
+        df = getPagos(df)
+        df.to_excel(f"test/{i}.xlsx", index=False)
         #print(url)
         #print(df2)
